6d2a700ba7bc7471b69fce00a7953d8962d4e2c6
Samuel Overton Jr <samovertonjr@gmail.com>
2018-07-12 22:51:02 -0500

    reverse string #2 solution

--- 1-reverse-string/app.js
+++ 1-reverse-string/app.js
@@ -1,8 +1,28 @@
-function reverseString(str) {
-  return str.split('').reverse().join('')
-}
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
 
-//ES2015
 
-const reverseStringAgain = (str) => str.split('').reverse().join('')
+// Example 1
+const reverseString = (str) => str.split('').reverse().join('');
+
+reverseString('apple');
+
+// Example 2
+const reverseString2 = (str) => {
+
+  let reversed = '';
+
+  for ( let char of str ) {
+    reversed = char + reversed;
+  }
+
+  return reversed;
+};
+
+reverseString2('Samuel');
 
