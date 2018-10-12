60c84ff9555365df02123877dfb2bafb98388adf
Samuel Overton Jr <samovertonjr@gmail.com>
2018-10-12 04:17:22 -0500

    longest word algorithm complete

--- ffc-longest-word/app.js
+++ ffc-longest-word/app.js
@@ -6,13 +6,15 @@ Your response should be a number.
 
 function findLongestWordLength(str) {
   const arrayStrings = str.split(' ');
-  const wordLength = 0;
+  let wordLength = 0;
 
   for (let i = 0; i < arrayStrings.length; i++) {
-    console.log(arrayStrings[i]);
+    if (arrayStrings[i].length > wordLength) {
+      wordLength = arrayStrings[i].length;
+    }
   }
 
-  return str.length;
+  return wordLength;
 }
 
 findLongestWordLength('The quick brown fox jumped over the lazy dog');
