f7174c93f5cab01447334f280d1db2b8e0e0c15c
Samuel Overton Jr <samovertonjr@gmail.com>
2018-11-01 23:01:18 -0500

    chunked algorithm v-2

--- 11-chunked/app.js
+++ 11-chunked/app.js
@@ -1,29 +1,49 @@
 // --- Directions
-// Write a program that console logs the numbers
-// from 1 to n. But for multiples of three print
-// “fizz” instead of the number and for the multiples
-// of five print “buzz”. For numbers which are multiples
-// of both three and five print “fizzbuzz”.
-// --- Example
-//   fizzBuzz(5);
-//   1
-//   2
-//   fizz
-//   4
-//   buzz
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function fizzBuzz(n) {
-	for (let i = 1; i <= n; i++) {
-		if (i % 15 === 0) {
-			console.log('fizbuzz');
-		} else if (i % 5 === 0) {
-			console.log('buzz');
-		} else if (i % 3 === 0) {
-			console.log('fizz');
-		} else {
-			console.log(i);
-		}
+// function chunk(array, size) {
+//   let chunked = [];
+
+//   for (let element of array) {
+//     const lastEl = chunked[chunked.length - 1];
+
+//     if (!lastEl || lastEl.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       lastEl.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+// function chunk(array, size) {
+//   const chunked = [];
+//   let index = 0;
+
+//   while (index < array.length) {
+//     chunked.push(array.slice(index, index + size));
+//     index += size;
+//   }
+//   return chunked;
+// }
+
+function chunk(array, size) {
+	const chunked = [];
+	debugger;
+	for (let i = 0; i < array.length; i = i + size) {
+		chunked.push(array.slice(i, i + size));
 	}
+	return chunked;
 }
 
-fizzBuzz(25);
+chunk([1, 2, 3, 4, 5, 6, 7], 4);
+
+module.exports = chunk;
