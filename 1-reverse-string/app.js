6aa74815872d31eb731829d64772a730b4b8b19d
Samuel Overton Jr <samovertonjr@gmail.com>
2018-11-07 01:40:35 -0600

    new reverse algorithm

--- 1-reverse-string/app.js
+++ 1-reverse-string/app.js
@@ -6,23 +6,37 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-
 // Example 1
-const reverseString = (str) => str.split('').reverse().join('');
+const reverseString = str =>
+	str
+		.split('')
+		.reverse()
+		.join('');
 
 reverseString('apple');
 
 // Example 2
-const reverseString2 = (str) => {
+const reverseString2 = str => {
+	let reversed = '';
 
-  let reversed = '';
+	for (let char of str) {
+		reversed = char + reversed;
+	}
 
-  for ( let char of str ) {
-    reversed = char + reversed;
-  }
-
-  return reversed;
+	return reversed;
 };
 
 reverseString2('Samuel');
 
+function reverseString3(str) {
+	if (!str || typeof str != 'string' || str.length < 2) return str;
+
+	const backwards = [];
+	const totalItems = str.length - 1;
+	for (let i = totalItems; i >= 0; i--) {
+		backwards.push(str[i]);
+	}
+	return backwards.join('');
+}
+
+reverseString2('Milia');
